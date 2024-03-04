5cc34b43a50e3b34d5f9d02434e0cba583aa3de6
x00217230237 <x00217230237@njit.edu.cn>
2024-03-04 19:38:08 +0800

    first commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public interface main {
+    public static void main(String[] args) throws Exception{
+
+        int[]array = {38,65,3,17,2};
+
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.bubbbleSort(array);
+    }
+
+}
